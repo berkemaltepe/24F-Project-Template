802e3d764c6ebabfa384ab4e248c0e68fe499b8c
Nick Ashizawa <nickashi2002@gmail.com>
2024-11-24 19:04:59 -0500

    changed home page

--- app/src/Home.py
+++ app/src/Home.py
@@ -42,19 +42,20 @@ st.write('## Select a user: ')
 # functionality, we put a button on the screen that the user 
 # can click to MIMIC logging in as that mock user. 
 
-if st.button("Act as Yuta, a Student", 
+if st.button("Act as Berke, a Student", 
             type = 'primary', 
             use_container_width=True):
     # when user clicks the button, they are now considered authenticated
     st.session_state['authenticated'] = True
     # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
+    st.session_state['role'] = 'student'
     # we add the first name of the user (so it can be displayed on 
     # subsequent pages). 
-    st.session_state['first_name'] = 'John'
+    st.session_state['first_name'] = 'Berke'
     # finally, we ask streamlit to switch to another page, in this case, the 
     # landing page for this particular user type
-    logger.info("Logging in as Political Strategy Advisor Persona")
+    logger.info("Logging in as Student Persona")
+    # CHANGE TO STUDENT HOME PAGE (make a new page or something like that)
     st.switch_page('pages/00_Pol_Strat_Home.py')
 
 # (Nick) currently working on pls don't change
@@ -62,25 +63,37 @@ if st.button('Act as Nick, an Employer',
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
-    st.session_state['role'] = 'usaid_worker'
-    st.session_state['first_name'] = 'Mohammad'
+    st.session_state['role'] = 'employer'
+    st.session_state['first_name'] = 'Nick'
     st.switch_page('pages/Emp_Home_Page.py')
 
-if st.button('Act as Lea, System Admin', 
+if st.button('Act as Yuta, Co-Op Advisor', 
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
-    st.session_state['role'] = 'administrator'
-    st.session_state['first_name'] = 'SysAdmin'
+    st.session_state['role'] = 'advisor'
+    st.session_state['first_name'] = 'Yuta'
+    # CHANGE TO ADVISOR HOME PAGE (make a new page or something like that)
     st.switch_page('pages/20_Admin_Home.py')
 
-if st.button("Act as Berke, a Co-op Advisor", 
+if st.button("Act as Lea, the Head of Khoury College of CS", 
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
-    st.session_state['role'] = 'pol_strat_advisor'
-    st.session_state['first_name'] = 'John'
-    logger.info("Logging in as Co-Op Advisor")
+    st.session_state['role'] = 'dept_head'
+    st.session_state['first_name'] = 'Lea'
+    logger.info("Logging in as Khoury Dept. Head")
+    # CHANGE TO DEPT HEAD HOME PAGE (make a new page or something like that)
+    st.switch_page('pages/00_Pol_Strat_Home.py')
+
+if st.button("Act as Colin, the System Admin", 
+            type = 'primary', 
+            use_container_width=True):
+    st.session_state['authenticated'] = True
+    st.session_state['role'] = 'sysadmin'
+    st.session_state['first_name'] = 'Colin'
+    logger.info("Logging in as System Admin")
+    # CHANGE TO SYS ADMIN HOME PAGE (make a new page or something like that)
     st.switch_page('pages/00_Pol_Strat_Home.py')
 
 
--- app/src/pages/Emp_Home_Page.py
+++ app/src/pages/Emp_Home_Page.py
@@ -9,17 +9,17 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome Nick, {st.session_state['first_name']}.")
+st.title(f"Welcome to the Employer Home Page, {st.session_state['first_name']}.")
 st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('Predict Value Based on Regression Model', 
+if st.button('View Your Job Listings (Currently prediction page temp)', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/11_Prediction.py')
 
-if st.button('View the Simple API Demo', 
+if st.button('View a list of your matched candidates (Currently Simple API Demo temp)', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/12_API_Test.py')
