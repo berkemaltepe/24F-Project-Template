115b599f55f32618b15d5f81e38b790c94327e7d
Nick Ashizawa <nickashi2002@gmail.com>
2024-11-27 16:18:19 -0500

    employer updates

--- app/src/pages/Emp_Home_Page.py
+++ app/src/pages/Emp_Home_Page.py
@@ -9,7 +9,8 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome to the Employer Home Page, {st.session_state['first_name']}.")
+## Different employer pages???
+st.title(f"Welcome to the Tech Corp Employer Home Page, {st.session_state['first_name']}.")
 st.write('')
 st.write('')
 st.write('### What would you like to do today?')
