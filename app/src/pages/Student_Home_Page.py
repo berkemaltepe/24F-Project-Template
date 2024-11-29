7fdc4ddcc4a3273b7cca45d9878f6dc7a7cf6cd2
Berke Maltepe <34270476+BerkeMaltepe@users.noreply.github.com>
2024-11-28 21:59:59 -0500

    spelling

--- app/src/pages/Student_Home_Page.py
+++ app/src/pages/Student_Home_Page.py
@@ -19,7 +19,7 @@ if st.button('View Job Listings',
              use_container_width=True):
   st.switch_page('pages/Student_Jobs.py')
 
-if st.button('View your profile', 
+if st.button('View Your profile', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/Student_Profile.py')
